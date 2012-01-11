f816b7d29cc9ba0bfbabcb54f76f47045e152890
Vivek Thampi <vivek.mt@gmail.com>
2012-01-10 21:52:14 -0800

    Add admin interface

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 from django.views.generic.simple import redirect_to
 import settings
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     ('^$', 
@@ -13,6 +16,8 @@ urlpatterns = patterns('',
         { 'template_name' : 'user_login.html' }),
     (r'^logout/$',
         'django.contrib.auth.views.logout'),
+    (r'^admin/',
+        include(admin.site.urls)),
     (r'^%s/(?P<path>.*)$' % settings.MEDIA_URL.strip('/'),
         'django.views.static.serve', 
         { 'document_root' : settings.MEDIA_ROOT }),
